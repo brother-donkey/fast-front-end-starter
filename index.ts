6f70a55cb7e4e0abded0a14d525db0c05589a735
Will Bjorn <willbjorn@gmail.com>
2020-07-11 22:29:31 -0700

    add app template

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import { FASTElement, customElement, attr, html, observable, when, repeat } from '@microsoft/fast-element';
 import { fetchRepoData, GitHubReposResponse, GitHubRepo } from './scripts/get-repo-data';
 
+const appTemplate = html<App>`
+  <h1>${x => x.heading}</h1>  
+  ${when(x => x.ready, repoTemplate)}
+`;
+
 @customElement({ name: 'the-app', template: appTemplate })
 export class App extends FASTElement {
   @attr heading: string = 'repos';
